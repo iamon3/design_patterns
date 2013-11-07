e545a0474b36d64311642a6fb93c6e0216848738
free coder <iamon3.free@gmail.com>
2013-11-08 00:18:49 +0530

    Creational Design Pattern : Object pool code.

---
+++ src/creational/objectPool/Main.java
@@ -0,0 +1,24 @@
+package creational.objectPool;
+
+import java.sql.Connection;
+
+public class Main {
+	public static void main(String args[]) {
+		// Do something...
+		//
+
+		// Create the ConnectionPool:
+		JDBCConnectionPool pool = new JDBCConnectionPool(
+				"org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+				"sa", "secret");
+
+		// Get a connection:
+		Connection con = pool.checkOut();
+
+		// Use the connection
+		//
+		
+		// Return the connection:
+		pool.checkIn(con);
+	}
+}
