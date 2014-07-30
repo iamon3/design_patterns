3004fbf0295c19b2fb7a1545881590ed5f7289d6
free coder <iamon3.free@gmail.com>
2014-07-30 17:43:20 +0530

    Mediator Pattern example.

---
+++ src/behavioral/mediator/eg1/MediatorDemo.java
@@ -0,0 +1,87 @@
+package behavioral.mediator.eg1;
+
+/**
+ * Difference between Mediator and Observer is 
+ * Observer addresses 1:M and Mediator address M:N communication
+ * 
+ * 1. Create an "intermediary" that decouples "senders" from "receivers"
+ * 2. Producers are coupled only to the Mediator
+ * 3. Consumers are coupled only to the Mediator
+ * 4. The Mediator arbitrates the storing and retrieving of messages
+ */
+
+class MediatorDemo {
+	public static void main( String[] args ) {
+		Mediator mb = new Mediator();
+		new Producer( mb ).start();
+		new Producer( mb ).start();
+		new Consumer( mb ).start();
+		new Consumer( mb ).start();
+		new Consumer( mb ).start();
+		new Consumer( mb ).start();
+	}
+}
+
+//1. The "intermediary"
+class Mediator {
+	// 4. The Mediator arbitrates
+	private boolean slotFull = false;
+	private int number;
+	public synchronized void storeMessage( int num ) {
+		// no room for another message
+		while (slotFull == true) {
+			try {
+				wait();
+			}
+			catch (InterruptedException e ) { }
+		}
+		slotFull = true;
+		number = num;
+		notifyAll();
+	}
+	public synchronized int retrieveMessage() {
+		// no message to retrieve
+		while (slotFull == false)
+			try {
+				wait();
+			}
+		catch (InterruptedException e ) { }
+		slotFull = false;
+		notifyAll();
+		return number;
+	}
+}
+
+class Producer extends Thread {
+	// 2. Producers are coupled only to the Mediator
+	private Mediator med;
+	private int    id;
+	private static int num = 1;
+	public Producer( Mediator m ) {
+		med = m;
+		id = num++;
+	}
+	public void run() {
+		int num;
+		while (true) {
+			med.storeMessage( num = (int)(Math.random()*100) );
+			System.out.print( "p" + id + "-" + num + "  " );
+		}
+	}
+}
+
+class Consumer extends Thread {
+	// 3. Consumers are coupled only to the Mediator
+	private Mediator med;
+	private int    id;
+	private static int num = 1;
+	public Consumer( Mediator m ) {
+		med = m;
+		id = num++;
+	}
+	public void run() {
+		while (true) {
+			System.out.print("c" + id + "-" + med.retrieveMessage() + "  ");
+		}
+	}
+}
